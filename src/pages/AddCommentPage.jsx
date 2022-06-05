fc91f5dffd16ac8afcb08591b3476c2766956ae3
FranciscoGBento <f_m_bento@hotmail.com>
2022-06-05 17:10:23 +0100

    comments and establish

---
+++ src/pages/AddCommentPage.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import axios from "axios";
+import { useParams, useNavigate } from "react-router-dom";
+
+function AddCommentPage() {
+  const [name, setName] = useState("");
+  const [comments, setComments] = useState("");
+
+  const { id } = useParams();
+  const navigate = useNavigate();
+
+  const handleName = (e) => setName(e.target.value);
+  const handleComments = (e) => setComments(e.target.value);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const body = { name, comments };
+
+    axios
+      .post(
+        `${process.env.REACT_APP_API_URL}/api/categories/type/${id}/comment`,
+        body
+      )
+      .then(() => {
+        setName("");
+        setComments("");
+        navigate("/");
+      });
+  };
+
+  return (
+    <div>
+      <h1>Give a comment of the service</h1>
+
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="name">Name:</label>
+        <input type="text" name="name" value={name} onChange={handleName} />
+
+        <label htmlFor="comments">Comment:</label>
+        <input
+          type="text"
+          name="comments"
+          value={comments}
+          onChange={handleComments}
+        />
+      </form>
+    </div>
+  );
+}
+
+export default AddCommentPage;
---
+++ src/pages/EstablishmentsPage.jsx
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import AddCommentPage from "./AddCommentPage";
+
+function EstablishmentsPage() {
+  const [establishments, setEstablishments] = useState([]);
+
+  const getAllEstablishments = async () => {
+    try {
+      let response = await axios.get(
+        `${process.env.REACT_APP_API_URL}/api/categories/type`
+      );
+      setEstablishments(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getAllEstablishments();
+  }, []);
+
+  return (
+    <div>
+      {establishments.map((establish) => {
+        <div key={establish._id}>
+          <Link to={`/establishment/${establish._id}`}>
+            <h4>{establish.companyName}</h4>
+          </Link>
+        </div>;
+      })}
+    </div>
+  );
+}
+
+export default EstablishmentsPage;
