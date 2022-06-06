8796e9c5418c86e5d83675c258a91fc7a0bf3e15
pedrocncosta <pedrocncosta@gmail.com>
2022-06-06 10:32:48 +0100

    add edit profile page

--- src/pages/EditProfilePage.jsx
+++ src/pages/EditProfilePage.jsx
@@ -2,9 +2,136 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useParams, useNavigate } from "react-router-dom";
 
-
 function EditProfilePage() {
-  return <div>EditProfile</div>;
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [profileType, setProfileType] = useState("");
+  const [comments, setComments] = useState("");
+  const [establishments, setEstablishments] = useState("");
+
+  const { userId } = useParams();
+  const navigate = useNavigate();
+
+  const getProfile = async () => {
+    try {
+      let response = await axios.get(
+        `http://localhost:5005/api/user/${userId}`
+      );
+      setProfileType(response.data.profileType);
+      setUsername(response.data.username);
+      setEmail(response.data.email);
+      setPassword(response.data.password);
+      setComments(response.data.comments);
+      setEstablishments(response.data.establishments);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const deleteProfile = async () => {
+    try {
+      await axios.delete(`http://localhost:5005/api/user/${userId}`);
+      navigate("/profile");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getProfile();
+  }, []);
+
+  const handleProfileType = (e) => setProfileType(e.target.value);
+  const handleUsername = (e) => setUsername(e.target.value);
+  const handleEmail = (e) => setEmail(e.target.value);
+  const handlePassword = (e) => setPassword(e.target.value);
+  const handleComments = (e) => setComments(e.target.value);
+  const handleEstablishments = (e) => setEstablishments(e.target.value);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const body = {
+      username,
+      email,
+      password,
+      comments,
+      establishments,
+      profileType,
+    };
+
+    axios
+      .put(`http://localhost:5005/api/projects/${projectId}`, body)
+      .then(() => {
+        setProfileType("");
+        setComments("");
+        navigate(`/projects/${projectId}`);
+      })
+      .catch((err) => console.log(err));
+  };
+
+  return (
+    <div className="EditProjectPage">
+      <h3>Edit the Project</h3>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="profileType">profileType</label>
+        <input
+          type="text"
+          name="profileType"
+          value={profileType}
+          onChange={handleProfileType}
+        />
+
+        <label htmlFor="username">Username</label>
+        <input
+          type="text"
+          name="username"
+          value={username}
+          onChange={handleUsername}
+        />
+
+        <label htmlFor="email">Email</label>
+        <input type="email" name="email" value={email} onChange={handleEmail} />
+
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          name="password"
+          value={password}
+          onChange={handlePassword}
+        />
+
+        <label htmlFor="profileType">profileType</label>
+        <input
+          type="text"
+          name="profileType"
+          value={profileType}
+          onChange={handleProfileType}
+        />
+
+        <label htmlFor="comments">Comments</label>
+        <textarea
+          name="comments"
+          cols="30"
+          rows="10"
+          value={comments}
+          onChange={handleComments}
+        ></textarea>
+
+        <label htmlFor="establishments">Establishments</label>
+        <input
+          type="text"
+          name="establishments"
+          value={establishments}
+          onChange={handleEstablishments}
+        />
+
+        <button type="submit">Edit</button>
+      </form>
+      <button onClick={deleteProfile}>Delete</button>
+    </div>
+  );
 }
 
 export default EditProfilePage;
